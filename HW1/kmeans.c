c741eebe67b9489344391a28ebbcebc49b5edadb
Troyansky1 <128223480+Troyansky1@users.noreply.github.com>
2024-12-10 17:10:58 +0200

    Added code from classroom
    Also replaced all "cord" with "coord"

---
+++ HW1/kmeans.c
@@ -0,0 +1,56 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+struct coord
+{
+    double value;
+    struct coord *next;
+};
+struct vector
+{
+    struct vector *next;
+    struct coord *coords;
+};
+
+int main(int argc, char **argv)
+{
+
+    struct vector *head_vec, *curr_vec, *next_vec;
+    struct coord *head_coord, *curr_coord, *next_coord;
+    int i, j, rows = 0, cols;
+    double n;
+    char c;
+
+    head_coord = malloc(sizeof(struct coord));
+    curr_coord = head_coord;
+    curr_coord->next = NULL;
+
+    head_vec = malloc(sizeof(struct vector));
+    curr_vec = head_vec;
+    curr_vec->next = NULL;
+
+
+    while (scanf("%lf%c", &n, &c) == 2)
+    {
+
+        if (c == '\n')
+        {
+            curr_coord->value = n;
+            curr_vec->coords = head_coord;
+            curr_vec->next = malloc(sizeof(struct vector));
+            curr_vec = curr_vec->next;
+            curr_vec->next = NULL;
+            head_coord = malloc(sizeof(struct coord));
+            curr_coord = head_coord;
+            curr_coord->next = NULL;
+            continue;
+        }
+
+        curr_coord->value = n;
+        curr_coord->next = malloc(sizeof(struct coord));
+        curr_coord = curr_coord->next;
+        curr_coord->next = NULL;
+    }
+
+    return 0;
+}
