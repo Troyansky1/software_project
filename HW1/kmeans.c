135898f5258ddbdb5e017a8aaa14f20e47fa4aa6
Troyansky1 <128223480+Troyansky1@users.noreply.github.com>
2024-12-10 18:06:06 +0200

    Added  structs and a few modifications to the code from classrom
    vector -> data points
    vec -> point
    main -> init_datapoints
    new struct centroids

--- HW1/kmeans.c
+++ HW1/kmeans.c
@@ -3,7 +3,7 @@
 
 int validate_input(int argc, char **argv);
 
-void init_datapoints(int argc, char **argv);
+void init_datapoints(int argc, char **argv, int k);
 
 void assign_to_clusters();
 
@@ -11,28 +11,44 @@ void update_centroids();
 
 int convergence();
 
-int euclid_dist(coord vector1, coord vetor2);
+int euclid_dist(coord data_point1, coord vetor2);
 
 void run_kmenas();
 
+void reset_centroids();
 
+
+
+// A value in a data point. next points to the next coordinate.
 struct coord
 {
     double value;
     struct coord *next;
 };
-struct vector
+
+// A data point with values, linked list of coords. 
+struct data_points
 {
-    struct vector *next;
+    struct data_points *next;
     struct coord *coords;
+    struct centroids *centroid; //We onlly need the one we point to, not the whole list.
 };
 
-int main(int argc, char **argv)
+struct centroids
 {
+    struct centroids *next;
+    struct coord *coords;
+    int cnt_points; // So we can calc the new mean
+    struct new_coords; // So we can add up coord and calc new mean
+}
 
-    struct vector *head_vec, *curr_vec, *next_vec;
+
+int init_datapoints(int argc, char **argv, int k)
+{
+    // Initialization
+    struct data_points *head_point, *curr_point, *next_point;
     struct coord *head_coord, *curr_coord, *next_coord;
-    int i, j, rows = 0, cols;
+    int i, j = 0, rows = 0, cols = 0;
     double n;
     char c;
 
@@ -40,33 +56,35 @@ int main(int argc, char **argv)
     curr_coord = head_coord;
     curr_coord->next = NULL;
 
-    head_vec = malloc(sizeof(struct vector));
-    curr_vec = head_vec;
-    curr_vec->next = NULL;
+    head_point = malloc(sizeof(struct data_point));
+    curr_point = head_point;
+    curr_point->next = NULL;
 
-
+    // Read input
     while (scanf("%lf%c", &n, &c) == 2)
     {
-
+        // last value in the coordinate. 
         if (c == '\n')
         {
             curr_coord->value = n;
-            curr_vec->coords = head_coord;
-            curr_vec->next = malloc(sizeof(struct vector));
-            curr_vec = curr_vec->next;
-            curr_vec->next = NULL;
+            curr_point->coords = head_coord;
+            curr_point->next = malloc(sizeof(struct data_point));
+            curr_point = curr_point->next;
+            curr_point->next = NULL;
             head_coord = malloc(sizeof(struct coord));
             curr_coord = head_coord;
             curr_coord->next = NULL;
+            rows ++;
             continue;
         }
-
+        // Add another value to same coordinate
         curr_coord->value = n;
         curr_coord->next = malloc(sizeof(struct coord));
         curr_coord = curr_coord->next;
         curr_coord->next = NULL;
+        j++
     }
-
+    cols = j / rows; // Calculate the num of columns as the total coords/rows
     return 0;
 }
 
